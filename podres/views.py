0d107632835aee8c91e2ba8618a1feb7d13d309b
imsiriuse <imsiriuse@gmail.com>
2023-01-27 14:55:12 +0100

    added basic views and index.html templates

--- podres/views.py
+++ podres/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Service, ServiceInstance
 
-# Create your views here.
+
+def index(request):
+    # Generate counts of some of the main objects
+    num_books = Service.objects.all().count()
+    num_instances = ServiceInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = ServiceInstance.objects.filter(active__exact=True).count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+    }
+
+    return render(request, 'index.html', context=context)
