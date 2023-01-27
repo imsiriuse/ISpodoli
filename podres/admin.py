2ae5e6bbdd0441fbe5d76be49ab5cdab9e75942b
imsiriuse <imsiriuse@gmail.com>
2023-01-27 13:08:55 +0100

    removed outsourced dj-booking app from project

--- podres/admin.py
+++ podres/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Service, ServiceInstance
 
-# Register your models here.
+
+@admin.register(ServiceInstance)
+class ServiceInstanceAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('label', 'service')
+        }),
+        ('Availability', {
+            'fields': ('active', 'closed')
+        }),
+    )
+
+
+class ServiceInstanceInline(admin.TabularInline):
+    model = ServiceInstance
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('label', 'calendar')
+    fields = ['label', 'calendar']
+    inlines = [ServiceInstanceInline]
--- podres/models.py
+++ podres/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Service(models.Model):
     label = models.CharField(max_length=20, help_text='Enter name')
+    calendar = models.CharField(max_length=20, default="")
 
     def __str__(self):
         return self.label
@@ -10,9 +12,18 @@ class Service(models.Model):
 
 class ServiceInstance(models.Model):
     label = models.CharField(max_length=20, help_text='Enter name')
+    service = models.ForeignKey(Service, on_delete=models.RESTRICT, null=False)
+    active = models.BooleanField(default=False)
+    closed = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ['label']
 
     def __str__(self):
         return self.label
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
 
 
 class Restriction(models.Model):
@@ -51,6 +62,13 @@ class TimeBlock(models.Model):
 
 
 class BlockSize(models.Model):
+    label = models.CharField(max_length=20, help_text='Enter name')
+
+    def __str__(self):
+        return self.label
+
+
+class UserRestriction(models.Model):
     label = models.CharField(max_length=20, help_text='Enter name')
 
     def __str__(self):
