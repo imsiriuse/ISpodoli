313d8245cb6a73c52102f4d875583743b2c93162
imsiriuse <imsiriuse@gmail.com>
2023-05-15 12:16:55 +0200

    edited about.html template

--- podres/urls.py
+++ podres/urls.py
@@ -4,14 +4,13 @@ from .views.servicedetail import ServiceDetailView
 from .views.createbooking import CreateBookingView
 from .views.deletebooking import DeleteBookingView
 from .views.bookinglist import BookingListView
-from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', service_list, name='service_list'),
-    path('accounts/profile/', TemplateView.as_view(template_name='static/profile.html'), name='profile'),
-    path('about/', TemplateView.as_view(template_name='static/about.html'), name='about'),
+    path('accounts/profile/', profile, name='profile'),
+    path('about/', about, name='about'),
     path('bookings/', BookingListView.as_view(), name='booking_list'),
     path('service/<int:pk>/', ServiceDetailView.as_view(), name='service_detail'),
     path('bookings/delete/<int:pk>/', DeleteBookingView.as_view(), name='delete_booking'),
--- podres/views/views.py
+++ podres/views/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from podres.models import Service, Booking
+from django.shortcuts import render
+from podres.models import Service, Booking, ServiceType
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
@@ -13,3 +13,11 @@ def service_list(request):
 def booking_history(request):
     bookings = Booking.objects.filter()
     return render(request, 'booking_history.html', {'bookings': bookings})
+
+def about(request):
+    service_types = ServiceType.objects.all()
+    return render(request, 'about.html', {'service_types': service_types})
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
